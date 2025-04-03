fd233cfdff7e2d1c93102579b7fa7a0867d6763d
QuyetBH203 <buihuuquyet203@gmail.com>
2025-04-03 11:42:14 +0700

    set up jwt token

--- src/main/java/org/example/audiobook/AudioBookApplication.java
+++ src/main/java/org/example/audiobook/AudioBookApplication.java
@@ -1,16 +1,23 @@
 package org.example.audiobook;
 
 
+import org.example.audiobook.component.JwtTokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class AudioBookApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AudioBookApplication.class, args);
     }
+
+
 
 
 
---
+++ src/main/java/org/example/audiobook/exception/InvalidParamException.java
@@ -0,0 +1,7 @@
+package org.example.audiobook.exception;
+
+public class InvalidParamException extends Exception {
+    public InvalidParamException(String message) {
+        super(message);
+    }
+}
