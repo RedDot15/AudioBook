6430e004e69e8935546e0a3f43f4980f2b7350fb
QuyetBH203 <buihuuquyet203@gmail.com>
2025-05-13 08:38:14 +0700

    add message

---
+++ src/main/java/org/example/audiobook/helper/NotificationMessage.java
@@ -0,0 +1,9 @@
+package org.example.audiobook.helper;
+
+
+public class NotificationMessage {
+    public static final String MESAGE_1 = "Hãy khám phá những sách mới nhất của chúng tôi!";
+
+    public static final String MESSAGE_2 = "Mỗi trang sách là một hành trình mới. Hãy tiếp tục khám phá!";
+
+}
--- src/main/java/org/example/audiobook/job/SendNotification.java
+++ src/main/java/org/example/audiobook/job/SendNotification.java
@@ -3,6 +3,7 @@ package org.example.audiobook.job;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.audiobook.entity.User;
+import org.example.audiobook.helper.NotificationMessage;
 import org.example.audiobook.service.UserService;
 import org.example.audiobook.service.fcm.FCMService;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,7 +22,7 @@ public class SendNotification {
     
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 90000)
     public void sendPeriodicNotifications() {
         try {
             log.info("Starting periodic notification job at: {}", LocalDateTime.now().format(formatter));
@@ -35,7 +36,7 @@ public class SendNotification {
             }
 
             String title = "AudioBook Update";
-            String body = "Hãy khám phá những sách mới nhất của chúng tôi! " + 
+            String body = NotificationMessage.MESAGE_1+" " +
                          LocalDateTime.now().format(formatter);
 
             int successCount = 0;
@@ -57,6 +58,44 @@ public class SendNotification {
             
         } catch (Exception e) {
             log.error("Error in periodic notification job: {}", e.getMessage());
+        }
+    }
+
+    @Scheduled(fixedRate = 120000)
+    public void sendMessageAccelerate(){
+        try {
+            log.info("Starting periodic notification accelerate job at: {}", LocalDateTime.now().format(formatter));
+
+
+            List<User> usersWithToken = userService.findUsersWithFCMToken();
+
+            if (usersWithToken.isEmpty()) {
+                log.info("No list user with FCM tokens found");
+                return;
+            }
+
+            String title = "AudioBook Update";
+            String body = NotificationMessage.MESSAGE_2+" " +
+                    LocalDateTime.now().format(formatter);
+
+
+            for (User user : usersWithToken) {
+                try {
+                    if (user.getFcmToken() != null && !user.getFcmToken().isEmpty()) {
+                        fcmService.sendNotification(title, body, user.getFcmToken());
+
+                    }
+                } catch (Exception e) {
+                    log.error("Failed to send message notification to user {}: {}",
+                            user.getId(), e.getMessage());
+                }
+            }
+
+            log.info("Completed sending notifications. Success"
+                    );
+
+        } catch (Exception e) {
+            log.error("Error in send message periodic notification job: {}", e.getMessage());
         }
     }
 }
